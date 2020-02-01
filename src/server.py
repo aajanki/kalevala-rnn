bc1d5a7c391afa9f308bd00e7deaeb4f1d121319
Antti Ajanki <antti.ajanki@iki.fi>
2020-02-01 17:06:51 +0200

    server side rendering

--- src/server.py
+++ src/server.py
@@ -1,15 +1,19 @@
 import os
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 from .sample import load_inference_model, character_generator, take
 
-app = FastAPI()
+app = FastAPI(title='Kalevalakone')
+templates = Jinja2Templates(directory='templates')
 model, char2idx, idx2char = load_inference_model(os.getenv('MODEL_PATH'))
 
 
 @app.get("/")
-async def root():
+async def root(request: Request):
     preseed = 'Mieleni minun tekevi'
     characters = character_generator(
         model, 0.2, idx2char, char2idx, preseed)
-    text = preseed + ''.join(take(200, characters))
-    return {'content': text}
+    verses = preseed + ''.join(take(300, characters))
+
+    return templates.TemplateResponse('main.html', {'request': request, 'verses': verses})
