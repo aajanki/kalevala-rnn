8d2f310a88bb3b8e873ca038c98180d4bebd1f2e
Antti Ajanki <antti.ajanki@iki.fi>
2020-02-15 17:09:03 +0200

    More cleanup, minimize vocabulary size

--- src/preprocess.py
+++ src/preprocess.py
@@ -16,9 +16,12 @@ def main():
         r"^\* \* \*$",
         r"^[A-ZÅÄÖ]+ KIRJA.?$",
         r"^[IV]+\.(?: \w+)+\.?$",
-        r"^\d+\.(?: [-\w'()]+,?)+[.?!]$",
+        r"^\d.$",
+        r"^\d+[.:](?: [-\w'().]+,?)+[.,?!]?$",
+        r"^[a-d]\)(?: [-\w'().]+,?)+[.,?!]?$",
         r"Yhteisiä Lauluja",
         r"Erityisiä Lauluja.",
+        r"Helsingissä \d+ päivä \w+ \d+.",
     ]
     section_title_re = re.compile('|'.join(section_titles), re.MULTILINE)
     parenthesis_re = re.compile(r'^\([\w .]+\)$', re.MULTILINE)
@@ -32,10 +35,24 @@ def main():
             text = section_title_re.sub('\n', text)
             text = parenthesis_re.sub('\n', text)
             text = multiemptyline_re.sub('\n\n', text)
+            text = text.replace(' (vaikka loppumattomaan)', '')
             while text.startswith('\n'):
                 text = text[1:]
             while text.endswith('\n'):
                 text = text[:-1]
+
+            # The replace some rare letters with more common letter
+            # (somewhat arbitrary) to minimize the vocabulary
+            text = (text
+                    .replace('b', 'p')
+                    .replace('c', 'k')
+                    .replace('d', 't')
+                    .replace('Ö', 'ö'))
+
+            # remove very rare letters (likely errors)
+            text = (text
+                    .replace('x', '')
+                    .replace('*', ''))
 
         with open(os.path.join(destdir, filename), 'w', encoding='utf-8-sig') as outf:
             outf.write(text)
