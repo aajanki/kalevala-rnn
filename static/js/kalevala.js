1844c57880dc8ec6499f2a0573522af51b537849
Antti Ajanki <antti.ajanki@iki.fi>
2020-02-22 19:00:59 +0200

    Some explanations

--- static/js/kalevala.js
+++ static/js/kalevala.js
@@ -8,7 +8,7 @@ function generate_verses(keywords) {
         return response.json();
     }).then(function(verses) {
         let verses_html = verses.join("\n<br>\n");
-        document.getElementById("poem-text").innerHTML = verses_html;
+        document.getElementById("verses").innerHTML = verses_html;
 
         document.getElementById("keywords-input").value = "";
     });
